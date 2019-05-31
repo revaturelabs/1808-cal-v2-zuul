c451781e39d4d300a65ffc583c9120fe27fab36e
Thomas Kok <kokthomas7016@gmail.com>
2019-05-31 15:51:39 -0400

    added a controller and connected a feign client in quality audit to category service

--- src/main/java/com/revature/caliber/zuul/CaliberZuulApplication.java
+++ src/main/java/com/revature/caliber/zuul/CaliberZuulApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @EnableZuulProxy
 @CrossOrigin("*")
 public class CaliberZuulApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CaliberZuulApplication.class, args);
 	}
